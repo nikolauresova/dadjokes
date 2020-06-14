b9b6184e8e49b7e236c606f24396d0cc54c6d0f6
Nikola Urešová <nikola.uresova@gmail.com>
2020-06-14 18:22:43 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,98 @@
 <template>
+<div class="app">
+  <Joke 
+    v-for="(joke, index) in jokes"
+    :user="joke.user"
+    :text="joke.text"
+    :likes="joke.likes"
+    :dislikes="joke.dislikes"
+    :key="index"
+    :idx="index"
+    :like="like($event)"
+    :downLike="downLike($event)"
   
-<div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <img class="joke__user" src="assets/img/user01.png" />
-          <p class="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"
-          </p>
-        </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up" @click="like"></button>
-          <span id="likes-up" class="likes-count likes-count--up">{{likes}}</span>
-          <button id="btn-down" class="btn-like btn-like--down" @click="downLike"></button>
-          <span id="likes-down" class="likes-count likes-count--down">{{downLikes}}</span>
-        </div>
-      </div>
-    </div>
-
+  />
+  </div>
 </template>
 
 <script>
+import Joke from "./Joke.vue";
+
 export default {
   name: 'App',
-  data () {
-    return {
-      likes: 0,
-      downLikes: 0
-    }
+  components: {
+    Joke:Joke
   },
   methods: {
-  like() {
-      this.likes++;
-    },
-    downLike () {
-      this.downLikes++;
+like(idx) {
+  this.jokes[idx].likes++
+},
+downLike(idx) {
+  this.jokes[idx].downLike++;
+}
+  },
+  data () {
+    return {
+      jokes: [
+        {
+          user: "user01",
+          text:`"The secret service isn't allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"`,
+          likes: 13,
+          dislikes: 24
+        },
+        {
+          user: "user02",
+          text: `Why did the invisible man turn down the job offer? He couldn't see himself doing it.`,
+          likes: 50,
+          dislikes: 10
+        },
+        {
+          user: "user03",
+          text: `I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!`,
+          likes: 30,
+          dislikes: 2
+        },
+        {
+          user:"user04",
+          text: `Did you hear the news? FedEx and UPS are merging. They’re going to go by the name Fed-Up from now on.`,
+          likes: 15,
+          dislikes: 8
+        },
+        {
+          user: "user05",
+          text: `"My daughter screeched, "Daaaaaad, you haven't listened to one word I've said, have you!?" What a strange way to start a conversation with me..."`,
+          likes: 90,
+          dislikes:44
+        },
+        {
+          user: "user06",
+          text: `My wife tried to unlatch our daughter's car seat with one hand and said, "How do one armed mothers do it?" Without missing a beat I replied, "Single handedly."`,
+          likes: 38,
+          dislikes: 1
+        },
+        {
+          user: "user07",
+          text: `"When a dad drives past a graveyard: Did you know that's a popular cemetery? Yep, people are just dying to get in there!"`,
+          likes: 66,
+          dislikes: 35
+        },
+        {
+          user: "user08",
+          text: `My friend keeps saying "cheer up man it could be worse, you could be stuck underground in a hole full of water." I know he means well.`,
+          likes: 27,
+          dislikes: 18
+        },
+        {
+          user: "user09",
+          text: `The fattest knight at King Arthur’s round table was Sir Cumference. He acquired his size from too much pi.`,
+          likes: 42,
+          dislikes: 9
+        }
+      ]
     }
   }
-
 }
 </script>
 
 <style scoped>
-
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: sans-serif;
-}
-
-.container {
-  max-width: 30rem;
-  margin: 5rem auto;
-}
-
-.joke {
-  margin-bottom: 2rem;
-}
-
-.joke__body {
-  display: flex;
-}
-
-.joke__user {
-  flex: 0 0 auto;
-  width: 5rem;
-  height: 5rem;
-  margin-right: 1rem;
-}
-
-.joke__text {
-  background-color: cornflowerblue;
-  color: white;
-  border-radius: 1rem;
-  padding: 1.5rem 2rem;
-  margin: 0;
-  line-height: 150%;
-}
-
-.joke__likes {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  margin-top: 1rem;
-}
-
-.likes-count {
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin: 0 0.5rem;
-}
-
-.likes-count--up {
-  color: #00e268;
-}
-
-.likes-count--down {
-  color: #ff5654;
-}
-
-.btn-like {
-  border: none;
-  outline: none;
-  width: 2rem;
-  height: 2rem;
-  border-radius: 1rem;
-  background-size: 1.2rem;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-.btn-like--up {
-  background-color: #00e268;
-  background-image: url('img/like-up.png');
-}
-
-.btn-like--down {
-  background-color: #ff5654;
-  background-image: url('img/like-down.png');
-}
-
 </style>
